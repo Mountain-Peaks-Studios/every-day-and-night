651673327b42d2a2b119e78a02a6bd52d448f60a
Versim <wojciechkrzos67@gmail.com>
2023-07-23 03:50:31 +0200

    feat: add basic PlayerController logic
    A basic player controller logic was added, including:
    - initialisation
    - health points
    - movement
    - shooting
    - taking damage
    - invincibility
    - dying mechanic

--- scenes/game/PlayerControl.gd
+++ scenes/game/PlayerControl.gd
@@ -1,28 +1,107 @@
+# This script represents a character body with shooting and health mechanics.
+
+# Extends the CharacterBody2D class.
 extends CharacterBody2D
 
+# Public variables that can be edited in the editor.
+# TODO: The InputMap action for shooting must be set up in the project settings.
+@export var speed: float = 200
+@export var bulletScene: PackedScene
+@export var bulletsLayer: int = 1
+@export var shootCooldown: float = 0.2
+@export var maxHealth: int = 100
+@export var invincibilityTime: float = 2.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# Private variables to handle movement, shooting, health, and invincibility.
+var customVelocity: Vector2 = Vector2.ZERO
+var canShoot: bool = true
+var currentHealth: int = maxHealth
+var invincible: bool = false
+var invincibilityTimer: float = 0.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# Called when the node enters the scene tree.
+func _ready():
+	# Enable physics processing.
+	set_physics_process(true)
+	# Initialize the character's health to its maximum value.
+	currentHealth = maxHealth
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+# Physics process, called every frame with a fixed delta time.
+func _physics_process(delta: float) -> void:
+	# Handle player input, shooting, and invincibility.
+	handleInput()
+	handleShooting(delta)
+	handleInvincibility(delta)
+	# Move the character using Godot's built-in move_and_slide function.
 	move_and_slide()
+
+# Handles player input to set the character's velocity.
+func handleInput() -> void:
+	# Reset the velocity to zero.
+	velocity = Vector2.ZERO
+
+	# Check for input actions and modify the velocity accordingly.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	# Normalize the velocity and multiply it by the speed to control movement speed.
+	velocity = velocity.normalized() * speed
+
+# Handles shooting mechanic, restricting the shooting rate with cooldown.
+func handleShooting(delta: float) -> void:
+	# Check if shooting is allowed and the "shoot" action is pressed.
+	if canShoot and Input.is_action_pressed("shoot"):
+		# Call the shoot function and start the shooting cooldown.
+		shoot()
+		canShoot = false
+		$shoot_timer.start(shootCooldown)
+
+# Timer callback to reset the canShoot variable after the cooldown has passed.
+func _on_shoot_timer_timeout() -> void:
+	canShoot = true
+
+# Shoots a bullet instance from the character.
+func shoot() -> void:
+	if bulletScene:
+		var bulletInstance = bulletScene.instance()
+		bulletInstance.position = position
+		bulletInstance.rotation = rotation
+		bulletInstance.set("owner", self)
+		get_parent().add_child(bulletInstance)
+
+# Reduces the character's health when taking damage.
+func takeDamage(damage: int) -> void:
+	# Check if the character is invincible and return if it is.
+	if invincible:
+		return
+
+	# Reduce the current health by the damage amount.
+	currentHealth -= damage
+
+	# Check if the character's health has dropped to or below zero, call the die function if true.
+	if currentHealth <= 0:
+		die()
+
+	# Activate invincibility and set the invincibility timer.
+	invincible = true
+	invincibilityTimer = invincibilityTime
+
+# Handles the invincibility mechanic, disabling it after a certain time.
+func handleInvincibility(delta: float) -> void:
+	if invincible:
+		# Reduce the invincibility timer.
+		invincibilityTimer -= delta
+		# Disable invincibility if the timer reaches or goes below zero.
+		if invincibilityTimer <= 0:
+			invincible = false
+
+# Handles the character's death logic.
+func die() -> void:
+	# TODO: Implement the end game logic here.
+	pass
