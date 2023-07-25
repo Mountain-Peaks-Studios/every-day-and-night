6593af42640bd23d052679138eb7d5fb27de589f
Versim <wojciechkrzos67@gmail.com>
2023-07-25 23:06:18 +0200

    fix: comment currently unused attack methods

--- scripts/day_mob_behaviour.gd
+++ scripts/day_mob_behaviour.gd
@@ -9,6 +9,7 @@ extends CharacterBody2D
 # Private variables
 var target: Node2D = null
 var attack_timer: float = 0.0
+signal deal_damage
 
 
 # Called when the node enters the scene tree.
@@ -20,7 +21,7 @@ func _ready():
 func _physics_process(delta):
 	update_target()
 	move_towards_target()
-	handle_attack(delta)
+	#handle_attack(delta)
 
 
 # Find the player, set it as the target
@@ -40,25 +41,25 @@ func move_towards_target():
 		move_and_slide()
 
 
-func handle_attack(delta):
-	if target:
-		# Calculate distance to the target to check if in-range
-		var distance_to_target = global_position.distance_to(target.global_position)
-		
-		# Check if target is within detectionRadius
-		if distance_to_target < detection_radius:
-			# Reduce attack cooldown timer
-			attack_timer =- delta
-			
-			# If the attack cooldown is over, perform the attack, reset the timer
-			if attack_timer <= 0.0:
-					handle_attack_helper()
-					attack_timer = attack_cooldown
-
-
-func handle_attack_helper():
-	# Send a damage signal to the target if chosen. 
-	# Replace "damage" with the signal name for taking damage in player script
-	if target:
-		target.emit_signal("take_damage", damage)
+#func handle_attack(delta):
+#	if target:
+#		# Calculate distance to the target to check if in-range
+#		var distance_to_target = global_position.distance_to(target.global_position)
+#
+#		# Check if target is within detectionRadius
+#		if distance_to_target < detection_radius:
+#			# Reduce attack cooldown timer
+#			attack_timer =- delta
+#
+#			# If the attack cooldown is over, perform the attack, reset the timer
+#			if attack_timer <= 0.0:
+#					handle_attack_helper()
+#					attack_timer = attack_cooldown
+#
+#
+#func handle_attack_helper():
+#	# Send a damage signal to the target if chosen. 
+#	# Replace "damage" with the signal name for taking damage in player script
+#	if target:
+#		target.emit_signal("deal_damage", damage)
 
