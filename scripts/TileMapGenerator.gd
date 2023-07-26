ca7d05caae2035d9d3c056c010ee20b991878af3
hehgwe <kacperopas0@gmail.com>
2023-07-26 15:49:49 +0200

    fix: fix rectangle and corridor generation

--- scripts/TileMapGenerator.gd
+++ scripts/TileMapGenerator.gd
@@ -10,12 +10,7 @@ var spawnY = 10
 	
 func _ready():
 	make_back_black()
-	
-	
 	map()
-	
-	
-	
 
 func make_back_black():
 	for x in width:
@@ -23,52 +18,51 @@ func make_back_black():
 			set_cell(0,Vector2(x,y),0 , Vector2(0,0))
 			
 
-func rectangle(locX: int, locY: int, offsetX: int, offsetY: int):
-	for x in locX:
-		for y in locY:
-			set_cell(0,Vector2(x+offsetX,y+offsetY),0,Vector2(8,5))
+func rectangle(locX: int, locY: int, sizeX: int, sizeY: int):
+	for x in sizeX:
+		for y in sizeY:
+			set_cell(0,Vector2(locX+x,locY+y),0,Vector2(8,5))
 
 func corridor(locX1: int, locY1: int, locX2: int, locY2: int):
 	
-	var counter = 4
+	var width = 4
+	# Horizontal
 	if locX2 - locX1 > locY2 - locY1:
 		for x in locX2 - locX1:
-			for c in counter:
-				set_cell(0,Vector2(locX1 + x,locY1 - 4 - c),0,Vector2(11,15))
+			for w in width:
+				set_cell(0,Vector2(locX1 + x,locY1 - 4 - w),0,Vector2(11,15))
+	# Vertical
 	else:
 		for y in locY2 - locY1:
-			for c in counter:
-				set_cell(0,Vector2(locX1 - 4 - c,locY1 + y),0,Vector2(11,15))
+			for w in width:
+				set_cell(0,Vector2(locX1 - 4 - w,locY1 + y),0,Vector2(11,15))
 	
 	
 func map():
 	#starting room
-	rectangle(10,10,spawnX,spawnY)
+	rectangle(spawnX, spawnY, 10, 10)
 	
 	#going right on x axis c times
 	drawRight(5)
-	
-	#going down on y axis c times
 	drawDown(5)
-	
+	drawRight(5)
 	drawDown(5)
-	
 	
 
 func drawRight(numberOfRooms: int):
 	for c in numberOfRooms:
-		corridor(spawnX*c + 10,spawnY + 10,30*c,10)
+		spawnX += 20
+		corridor(spawnX - 10, spawnY + 10, spawnX, spawnY + 10)
 		
-		rectangle(10,10,spawnX+20*(c+1),spawnY)
-	
-	spawnX = spawnX * ((numberOfRooms * 2) + 1)   
+		rectangle(spawnX, spawnY, 10, 10)
+		
 
 func drawDown(numberOfRooms: int):
 	for c in numberOfRooms:
-		corridor(spawnX + 10, spawnY*c + 10, 10, 30*c )
+		spawnY += 20
+		corridor(spawnX + 10, spawnY - 10, spawnX + 10, spawnY)
 		
-		rectangle(10,10,spawnX,spawnY+20*(c+1))
-	spawnY = spawnY * ((numberOfRooms * 2) + 1) 
+		rectangle(spawnX, spawnY, 10, 10)
 
 func make_a_random_box():
 	var margin := 10
@@ -87,4 +81,3 @@ func _input(event: InputEvent):
 	#	get_tree().reload_current_scene()
 		set_cell(1, Vector2(0,iterator),0, Vector2(8,5))
 		iterator += 1
-	
