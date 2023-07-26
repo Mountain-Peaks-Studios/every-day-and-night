455a4a1d6473a49e5dc66f9c146ef9d3e45beaf5
Condotierro <118808096+Condotierro@users.noreply.github.com>
2023-07-26 16:23:56 +0200

    feat : added possibility for all directions of generation using functions with number of rooms

--- scripts/TileMapGenerator.gd
+++ scripts/TileMapGenerator.gd
@@ -23,7 +23,7 @@ func rectangle(locX: int, locY: int, sizeX: int, sizeY: int):
 		for y in sizeY:
 			set_cell(0,Vector2(locX+x,locY+y),0,Vector2(8,5))
 
-func corridor(locX1: int, locY1: int, locX2: int, locY2: int):
+func positiveCorridor(locX1: int, locY1: int, locX2: int, locY2: int):
 	
 	var width = 4
 	# Horizontal
@@ -37,7 +37,20 @@ func corridor(locX1: int, locY1: int, locX2: int, locY2: int):
 			for w in width:
 				set_cell(0,Vector2(locX1 - 4 - w,locY1 + y),0,Vector2(11,15))
 	
+func negativeCorridor(locX1: int, locY1: int, locX2: int, locY2: int):
 	
+	var width = 4
+	# Horizontal
+	if locX2 - locX1 > locY2 - locY1:
+		for x in locX2 - locX1:
+			for w in width:
+				set_cell(0,Vector2(locX1 + x + 20,locY1 - 4 - w),0,Vector2(11,15))
+	# Vertical
+	else:
+		for y in locY2 - locY1:
+			for w in width:
+				set_cell(0,Vector2(locX1 - 4 - w,locY1 + y + 20),0,Vector2(11,15))
+
 func map():
 	#starting room
 	rectangle(spawnX, spawnY, 10, 10)
@@ -45,36 +58,37 @@ func map():
 	#going right on x axis c times
 	drawRight(5)
 	drawDown(5)
-	drawRight(5)
-	drawDown(5)
+	drawLeft(5)
+	drawUp(5)
 	
 
 func drawRight(numberOfRooms: int):
 	for c in numberOfRooms:
 		spawnX += 20
-		corridor(spawnX - 10, spawnY + 10, spawnX, spawnY + 10)
+		positiveCorridor(spawnX - 10, spawnY + 10, spawnX, spawnY + 10)
 		
 		rectangle(spawnX, spawnY, 10, 10)
-		
 
 func drawDown(numberOfRooms: int):
 	for c in numberOfRooms:
 		spawnY += 20
-		corridor(spawnX + 10, spawnY - 10, spawnX + 10, spawnY)
+		positiveCorridor(spawnX + 10, spawnY - 10, spawnX + 10, spawnY)
 		
 		rectangle(spawnX, spawnY, 10, 10)
 
-func make_a_random_box():
-	var margin := 10
-	var xOffest := randi_range(margin, width-margin)
-	var yOffest := randi_range(margin, height-margin)
-	
-	var xRoom := randi_range(10,30)
-	var yRoom := randi_range(10,30)
-	for x in xRoom:
-		for y in yRoom:
-			set_cell(0,Vector2(x+xOffest,y+yOffest), 0, Vector2(8,5))
+func drawLeft(numberOfRooms: int):
+	for c in numberOfRooms:
+		spawnX -= 20
+		negativeCorridor(spawnX - 10, spawnY + 10, spawnX, spawnY + 10)
+		
+		rectangle(spawnX, spawnY, 10, 10)
 
+func drawUp(numberOfRooms: int):
+	for c in numberOfRooms:
+		spawnY -= 20
+		negativeCorridor(spawnX + 10, spawnY - 10, spawnX + 10, spawnY)
+		
+		rectangle(spawnX, spawnY, 10, 10)
 
 func _input(event: InputEvent):
 	if event.is_action_pressed("scene_reload"):
