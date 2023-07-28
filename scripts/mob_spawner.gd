e8e082d1c7531dd8e4d5b76f3ef85fe94a70de4a
Versim <wojciechkrzos67@gmail.com>
2023-07-28 18:06:38 +0200

    feat: add basic spawning mechanic dependent on is_day boolean from game_controller

--- scripts/mob_spawner.gd
+++ scripts/mob_spawner.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@export var DAYMOB: PackedScene = preload("res://scenes/game/enemy/base_day_mob.tscn")
+@export var NIGHTMOB: PackedScene = preload("res://scenes/game/enemy/base_night_mob.tscn")
+
+var mob_to_spawn
+var is_day
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +13,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	spawn_enemy()
+
+
+func spawn_enemy():
+	
+	is_day = get_parent().get("is_day")
+	
+	if is_day:
+		mob_to_spawn = DAYMOB.instantiate()
+	else:
+		mob_to_spawn = NIGHTMOB.instantiate()
+	
+	self.get_parent().add_child(mob_to_spawn)
+	mob_to_spawn.global_position = self.global_position
