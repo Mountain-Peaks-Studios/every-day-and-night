b24678834d41999f5d678fbf27543287bed01665
Versim <wojciechkrzos67@gmail.com>
2023-07-28 18:07:48 +0200

    ref: cleanup, speed test

--- base_night_mob.gd
+++ base_night_mob.gd
@@ -1,28 +1,13 @@
 extends "res://scripts/base_mob_behaviour.gd"
 
+var night_speed = 100
+var night_isNight = 1
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+func _ready():
+	self.speed = night_speed
+	self.isNight = night_isNight
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	update_target()
+	update_health()
+	move_towards_target()
