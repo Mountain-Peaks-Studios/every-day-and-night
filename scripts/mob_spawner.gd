279af3ebef5f827184d02c46eaa9500392f34390
Versim <wojciechkrzos67@gmail.com>
2023-07-29 00:24:55 +0200

    feat: improve mob spawning
    - add random position in a ring
    - add random position in a circle
    - add horde spawning
    - choosing an enemy to spawn can now be randomised
    - the centre of spawning can be attached to the player

--- scripts/mob_spawner.gd
+++ scripts/mob_spawner.gd
@@ -8,41 +8,63 @@ var is_day
 
 @onready var spawn_timer = $SpawnTimer
 var can_spawn: bool = true
+var outer_radius: float = 600
+var inner_radius: float = 500
 
 signal spawn
 
-# Called when the node enters the scene tree for the firt time.
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	handle_input()
+	handle_input(delta)
 
-func handle_input():
-	if Input.is_action_pressed("temporary_action"):
+func handle_input(delta):
+	if can_spawn and Input.is_action_pressed("temporary_action"):
+		can_spawn = false
 		var horde = generate_horde()
 		spawn_horde(horde)
 
 func generate_horde() -> Array:
-	var test_horde = [DAYMOB, DAYMOB]
+	# Generate an array containing different enemy scenes randomly
+	var possible_enemies = [DAYMOB, NIGHTMOB]
+	var test_horde = []
+	
+	for number in range(50):  # You can modify the number of enemies in the horde here
+		var random_index = randi() % possible_enemies.size()
+		test_horde.append(possible_enemies[random_index])
+	
+	print("Horde generated!")
 	return test_horde
-
 
 func spawn_horde(input_array: Array):
 	for enemy in input_array:
 		spawn_enemy(enemy)
-
+		print("Horde spawned!")
 
 func spawn_enemy(enemy: PackedScene):
-	
-	if can_spawn:
-		var spawn_enemy = enemy.instantiate()
-		self.get_parent().add_child.call_deferred(spawn_enemy)
-		spawn_enemy.global_position = self.global_position
-		spawn_timer.start()
-		can_spawn = false
+	var spawn_enemy = enemy.instantiate()
+	self.get_parent().add_child(spawn_enemy)
+	spawn_enemy.global_position = get_random_pos_in_ring(outer_radius, inner_radius)
+	spawn_timer.start()
+	print("Enemy spawned!")
+
+func get_random_pos_in_ring(outer_radius: float, inner_radius: float) -> Vector2:
+	var x1 = randf_range(-1, 1)
+	var x2 = randf_range(-1, 1)
+
+	while x1 * x1 + x2 * x2 >= 1:
+		x1 = randf_range(-1, 1)
+		x2 = randf_range(-1, 1)
+
+	var random_pos_on_unit_circle = Vector2(2 * x1 * sqrt(1 - x1 * x1 - x2 * x2),
+											2 * x2 * sqrt(1 - x1 * x1 - x2 * x2))
+
+	var random_pos_in_ring = random_pos_on_unit_circle * randf_range(inner_radius, outer_radius)
+
+	return random_pos_in_ring
 
 
 func _on_spawn_timer_timeout():
