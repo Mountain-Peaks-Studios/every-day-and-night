71504cbfcf990a4a0129e3d8368ada0919782654
Versim <wojciechkrzos67@gmail.com>
2023-07-29 23:38:03 +0200

    feat: add basic pick-up logic

--- scenes/game/item/coin_behaviour.gd
+++ scenes/game/item/coin_behaviour.gd
@@ -9,3 +9,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_collider_area_entered(area):
+	if area.is_in_group("Player"):
+		self.get_parent().get_node("Player").get("number_of_coins") + 1
