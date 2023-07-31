bb6faba9a9743d3415a36827ff72b042c37ae105
Versim <wojciechkrzos67@gmail.com>
2023-07-31 16:26:10 +0200

    fix: coin now checks for the name of area that has entered its collider

--- scripts/game/item/coin_behaviour.gd
+++ scripts/game/item/coin_behaviour.gd
@@ -3,6 +3,6 @@ extends Node2D
 var amount = 1
 
 func _on_collider_area_entered(area) -> void:
-	if area.get_parent().add_coins(amount):
+	if area.name == "PlayerHurtbox":
+		area.get_parent().add_coins(amount)
 		queue_free()
-	queue_free()
